b7bbc3dac76551111b33a8e073d7123a8ab3b6a7
alexz <alexz@alexz.ximad.local>
2019-04-03 18:32:44 +0400

    update

--- bot2.js
+++ bot2.js
@@ -3,20 +3,8 @@
 const TeleBot = require('telebot');
 var dateFormat = require('dateformat');
 var newSet = require('./config.js');
-/*
-const newSet = {
-// iwantsome
-	token: '791540308:AAHVEHwF2yr0ARFEarBGCGVDlVco1stOg1c',
-	polling: { interval: 1000, limit: 1, timeout: 5, retryTimeout: 5000},
+var a = 1;
 
-// jetp1l0t_bot
-//	token: '866834920:AAHKusBh8qQaIHd7pTfK5tXb4KQwBV9hUa8',
-// polling: { interval: 1000, limit: 1, timeout: 5, retryTimeout: 5000, proxy: 'http://pu:pu@de.trolo.bid:14448' },
-
-	// webhook:  { url: 'https://trolo.bid/zhelezka', host: 'localhost', port: 3000 },
-	usePlugins: ['ignoreMessagesBeforeStart','floodProtection']
-	};
-*/
 const where = './pics/';
 bot = new TeleBot(newSet);
 
@@ -54,24 +42,21 @@ return msg.reply.text('пидарасы!',{ asReply: true });
 });
 
 //bot.on(/(п\s)?си*/, (msg) => {
-bot.on(/(да\sили\sнет\?)/, (msg) => {	
-return msg.reply.photo(choosePic(''),{ asReply: true });
+//bot.on(/(да\sили\sнет\?)/, (msg) => {
+bot.on(/^(exact\soption\?|another_option|да\sили\sнет\?)/, (msg) => {
+		return msg.reply.photo(choosePic('yesno',msg.chat.id,msg.from.username,msg.from.id), { asReply: true });
 });
 
-bot.on(/(арбайт)/i, (msg) => {
-return msg.reply.photo(choosePic('arbeit'), { asReply: true });
+bot.on(/(арбайт|работать)/i, (msg) => {
+	return msg.reply.photo(choosePic('arbeit',msg.chat.id,msg.from.username,msg.from.id), { asReply: true });
 });
 
-bot.on(/(работать)/i, (msg) => {
-return msg.reply.photo(choosePic('arbeit'), { asReply: true });
-});
-
-bot.on(/(попа)/i, (msg) => {
+bot.on(/(попа|жопа|задница)/i, (msg) => {
 	console.log(msg.chat.id + ' ' + msg.from.username + ' ' + msg.from.id);
 return msg.reply.photo(choosePic('popki',msg.chat.id,msg.from.username,msg.from.id), { asReply: true });
 });
 
-bot.on(/(сиси)/i, (msg) => {
+bot.on(/(сиси|сиськи)/i, (msg) => {
 return msg.reply.photo(choosePic('siski',msg.chat.id,msg.from.username,msg.from.id),{ asReply: true });
 });
 
@@ -79,4 +64,17 @@ bot.on(/(дева)/i, (msg) => {
 return msg.reply.photo(choosePic('full',msg.chat.id,msg.from.username,msg.from.id),{ asReply: true });
 });
 
+bot.on(/(gif)/i, (msg) => {
+	return bot.sendDocument(
+			msg.chat.id,
+			choosePic('gifs/poke',msg.chat.id,msg.from.username,msg.from.id),
+			{replyToMessage: msg.message_id}
+	)
+});
+
 bot.start();
+
+process.on('unhandledRejection', (reason, p) => {
+	console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+	// application specific logging, throwing an error, or other logic here
+  });
